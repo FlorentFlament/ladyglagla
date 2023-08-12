8381bc3e2faf55f2cbbb55d20390c64ead013fdf
Florent Flament <contact@florentflament.com>
2023-08-12 12:55:03 +0200

    fx first draft

--- hello.s
+++ hello.s
@@ -15,20 +15,75 @@ main:
 	trap	#14		; XBIOS trap
 	addq.l	#6,sp
 
+        ;; Hide mouse with a line A function
+        dc.w    $A00A
+
         ;; Copy picture data to video memory
         move.l  #picture_logo_data,a1
-        move.w  #32000-4,d1
-.loop:
-        move.l  (a1,d1.w),(a6,d1.w)
-        sub.w   #4,d1
-        bpl     .loop
+        move.w  #32000-4,d0
+.pic_loop:
+        move.l  (a1,d0.w),(a6,d0.w)
+        subq.w  #4,d0
+        bpl     .pic_loop
 
         ;; Wait for any key press then return
 	move.w	#8,-(sp)	; Cnecin
 	trap	#1		; GEMDOS
         addq.l	#2,sp
+
+        move.w  #5000-1,d7      ; Rotate 5000 lines
+        ;; Rotate one line
+.line_loop:
+	move.w	#17,-(sp)	; random
+	trap	#14		; XBIOS trap
+        addq.l	#2,sp
+        and.l   #$00ff,d0       ; % 256
+
+        cmp.w   #200,d0         ; % 200
+        bmi     .mod_200
+        sub.w   #200,d0
+
+.mod_200:
+        asl.w   #5,d0           ; *32
+        move.w  d0,a0
+        asl.w   #2,d0           ; *128
+        add.w   d0,a0           ; *160
+        add.l   a6,a0
+
+;        move.l  a6,a0
+;        add.w   #100*160,a0
+        jsr     line_shift_left
+        dbra    d7,.line_loop
+
+        ;; Wait for any key press then return
+	move.w	#8,-(sp)	; Cnecin
+	trap	#1		; GEMDOS
+        addq.l	#2,sp
+
+        ;; Display mouse with a line A function
+        dc.w    $A009
+
 	clr.w	-(sp)		; Pterm0
 	trap	#1		; GEMDOS
+
+;;; Shifts one line to the left
+;;; Address of beginning of line must be passed in a0
+;;; d0,d1,a1 are used
+line_shift_left::
+        move.w  #2,d1
+.bitplanes_loop:
+        move.w  #160,a1       ; 160 bytes per line
+        sub.w   d1,a1
+.rotate_loop:
+        roxl.w  (a0,a1)         ; Rotate video block
+        suba.w  #8,a1           ; Fix deplacement without updating SR flags
+        move.w  a1,d0           ; Move deplacement and test negativity
+        btst.l  #15,d0          ;
+        beq     .rotate_loop
+        addq.w  #2,d1
+        cmpi.w  #8,d1
+        ble     .bitplanes_loop
+        rts
 
         section bss,bss
         ;; Put variables here
