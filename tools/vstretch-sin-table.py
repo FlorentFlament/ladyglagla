21532a345b603f8505f3222df3fc363cc6c83a00
Florent Flament <contact@florentflament.com>
2023-11-13 17:05:39 +0100

    Update wave_ratio/pic_ratio table

--- tools/vstretch-sin-table.py
+++ tools/vstretch-sin-table.py
@@ -8,7 +8,8 @@ from asmlib import render
 
 
 def sin_func(x):
-    return 75*math.sin(2*math.pi/TABLE_LEN * x - 0.3398) + 125
+    #return 75*math.sin(2*math.pi/TABLE_LEN * x - 0.3398) + 125
+    return 90*math.sin(-2*math.pi/TABLE_LEN * x - 0.1113) + 110
 
 def sin_table():
     return [round(sin_func(x)) for x in range(TABLE_LEN)]
